f2d128176ad004e89d11657cfb7535a5b109449c
PaulSenon <paul.senon@iut2.upmf-grenoble.fr>
2017-06-11 20:00:16 +0200

    correct some issues

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    socket.emit('createEmail', {
-        to: 'paul.senon@gmail.com',
-        text: 'hey !'
-    });
-
     socket.emit('createMessage', {
         from: 'Jean-monique',
         text: 'Salut !'
@@ -18,10 +13,6 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function(email) {
-    console.log('newEmail', email);
-});
-
 socket.on('newMessage', function(message) {
-    console.log(message);
+    console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -15,24 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'monique@pouet.com',
-        text: 'hey i\'m monique :D',
-        createdAt: 123
-    });
-
-    socket.on('createEmail', (email) => {
-        console.log(JSON.stringify(email, undefined, 2));
+    socket.emit('newMessage', {
+        from: 'José',
+        text: 'hey i\'m josé :D',
+        createdAt: (new Date).getTime()
     });
 
     socket.on('createMessage', (message) => {
-        console.log(message);
-        timeStamp = (new Date).getTime();
-        socket.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: timeStamp
-        });
+        console.log('createMessage',message);
+        // timeStamp = (new Date).getTime();
+        // socket.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: timeStamp
+        // });
     });
 
     socket.on('disconnect', () => {
